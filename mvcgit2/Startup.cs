f0819a83b048a5e35f480b45c8034458f7f93b10
santiago.campero <santiago.campero@mediaccess.com.mx>
2017-11-21 11:38:56 -0600

    se sube proyecto por primera vez

---
+++ mvcgit2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcgit2.Startup))]
+namespace mvcgit2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
